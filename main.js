4bcdeed9b2e1094a9ce1756187040c5d766d47db
gouzmi <guillaume.dupont.rm@gmail.com>
2019-02-16 15:43:41 +0100

    d

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 var panSpeed = 1;
 var gravity = 0.3;
 var player;
+var stop = false;
+var frameCount = 0;
+var fps, fpsInterval, startTime, now, then, elapsed;
 
 function setup(){
-
   player = new Player(10,canvas.height/2);
   pipe = new Pipe();
-  draw();
+  startAnimating(60);
 }
 
 function draw() {
@@ -22,6 +24,37 @@ function draw() {
 document.addEventListener('keypress', (event) => {
   player.moveUp();
 });
+
+
+function startAnimating(fps) {
+  fpsInterval = 1000 / fps;
+  then = Date.now();
+  startTime = then;
+  animate();
+}
+
+function animate() {
+
+
+  requestAnimationFrame(animate);
+
+  // calc elapsed time since last loop
+  now = Date.now();
+  elapsed = now - then;
+  // if enough time has elapsed, draw the next frame
+
+  if (elapsed > fpsInterval) {
+      // Get ready for next frame by setting then=now, but also adjust for your
+      // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
+      then = now - (elapsed % fpsInterval);
+
+      pipe.update();
+      pipe.show();
+      player.update();
+      player.show();
+
+  }
+}
 
 
 
